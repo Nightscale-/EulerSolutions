b9ede85925cbd82204f4bf90b37d1791e22d1b2a
Jarrett <jjb_prof@hotmail.com>
2014-02-22 21:27:11 -0600

    finished gui and am able to collect needed information     need to complete calculation funtion to finish problem

--- src/com/eulersolutions/controllers/MultiplesAB.java
+++ src/com/eulersolutions/controllers/MultiplesAB.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MultiplesAB extends Activity {
 
@@ -22,9 +24,36 @@ public class MultiplesAB extends Activity {
 		return true;
 	}
 	
-	public void calculate (View view)
+	public void startCalculation (View view)
 	{
+		EditText textReader;
+		TextView textSolution;
+		int firstMultiple = 0;
+		int secondMultiple = 0;
+		int maxValue = 0;
+		long answer = 0;
 		
+		textReader = (EditText) this.findViewById(R.id.prob1FirstMultipleEditText);
+		firstMultiple = Integer.parseInt(textReader.getText().toString());
+		
+		textReader = (EditText) this.findViewById(R.id.prob1SecondMultipleEditText);
+		secondMultiple = Integer.parseInt(textReader.getText().toString());
+		
+		textReader = (EditText) this.findViewById(R.id.prob1MaxEditText);
+		maxValue = Integer.parseInt(textReader.getText().toString());
+		
+		answer = calculate(firstMultiple, secondMultiple, maxValue);
+		textSolution = (TextView) this.findViewById(R.id.prob1SolutionTextView);
+		textSolution.setText( answer + "");
+	}
+	
+	//No need for a model in this case because the entire solution is
+	//    one straight forward formula
+	public long calculate(int firstMultiple, int secondMultiple, int maxValue)
+	{
+		int toReturn = 0;
+		
+		return toReturn;
 	}
 
 }
