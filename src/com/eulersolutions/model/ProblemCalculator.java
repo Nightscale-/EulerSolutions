eada6be5712638be738ebfd01474d57d9d607331
Jarrett <jjb_prof@hotmail.com>
2014-02-26 12:44:50 -0600

    made ProblemSummary parcelable and added parcelable to the    ProblemCalculator as well. Need to implement it in the    calculator now.

--- src/com/eulersolutions/model/ProblemCalculator.java
+++ src/com/eulersolutions/model/ProblemCalculator.java
@@ -2,7 +2,9 @@ package com.eulersolutions.model;
 
 import java.util.List;
 
-public abstract class ProblemCalculator {
+import android.os.Parcelable;
+
+public abstract class ProblemCalculator implements Parcelable{
 	
 	protected abstract boolean validInput(List<Number> values);
 	protected abstract Number calculate(List<Number> values);
--- src/com/eulersolutions/model/ProblemSummary.java
+++ src/com/eulersolutions/model/ProblemSummary.java
@@ -1,39 +1,65 @@
 package com.eulersolutions.model;
 
-import android.content.Intent;
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class ProblemSummary implements Parcelable{
-	private String name;
-	private int id;
-	private Intent problem;
+	private String mName;
+	private String mDescription;
+	private String mExample;
+	private ProblemCalculator mCalculator;
+	private int mNumberOfInputs;
+	private int mId;
 	
-	public ProblemSummary(int newId, String newName, Intent newProblem)
+	public ProblemSummary(String name, String description, String example, ProblemCalculator calculator,
+			int numberOfInputs, int id)
 	{
-		name = newName;
-		id = newId;
-		problem = newProblem;
+		mName = name;
+		mDescription = description;
+		mExample = example;
+		mCalculator = calculator;
+		mNumberOfInputs = numberOfInputs;
+		mId = id;
 	}
 	
 	public ProblemSummary(Parcel source)
 	{
-		//TODO: fill in after reformating the internal data.
+		mName = source.readString();
+		mDescription = source.readString();
+		mExample = source.readString();
+		mCalculator = source.readParcelable(ProblemCalculator.class.getClassLoader());
+		mNumberOfInputs = source.readInt();
+		mId = source.readInt();
 	}
 	
 	public String getName()
 	{
-		return name;
+		return mName;
+	}
+	
+	public String getDescription()
+	{
+		return mDescription;
+	}
+	
+	public String getExample()
+	{
+		return mExample;
+	}
+	
+	public ProblemCalculator getCalculator()
+	{
+		return mCalculator;
+	}
+	
+	public int getNumberOfInputs()
+	{
+		return mNumberOfInputs;
 	}
 	
 	public int getId()
 	{
-		return id;
-	}
-	
-	public Intent getProblem()
-	{
-		return problem;
+		return mId;
 	}
 
 	@Override
@@ -43,7 +69,13 @@ public class ProblemSummary implements Parcelable{
 
 	@Override
 	public void writeToParcel(Parcel destination, int flags) {
-		//TODO: implement after updating the Summary information.
+		
+		destination.writeString(mName);
+		destination.writeString(mDescription);
+		destination.writeString(mExample);
+		destination.writeParcelable(mCalculator, flags);
+		destination.writeInt(mNumberOfInputs);
+		destination.writeInt(mId);
 	}
 	
 	public static final Parcelable.Creator<ProblemSummary> CREATOR = 
