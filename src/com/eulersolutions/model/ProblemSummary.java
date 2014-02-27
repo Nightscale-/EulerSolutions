b8dbb0b2ad3e76f6f82e949e8954fa2f3d4b1491
Jarrett <jjb_prof@hotmail.com>
2014-02-27 09:00:10 -0600

    updating model to handle dynamic number of inputs    and texts. continuing

--- src/com/eulersolutions/model/ProblemSummary.java
+++ src/com/eulersolutions/model/ProblemSummary.java
@@ -1,35 +1,40 @@
 package com.eulersolutions.model;
 
+import java.util.ArrayList;
+
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class ProblemSummary implements Parcelable{
-	private String mName;
-	private String mDescription;
-	private String mExample;
-	private String mSolution;
+	
+	public final static int NAME_INDEX = 0;
+	public final static int DESCRIPTION_INDEX = 1;
+	public final static int EXAMPLE_INDEX = 2;
+	public final static int SOLUTION_INDEX = 3;
+	public final static int FIRST_INPUT_INDEX = 4;
+	
+	private ArrayList<String> mTextStrings;
 	private ProblemCalculator mCalculator;
 	private int mNumberOfInputs;
 	private int mId;
 	
-	public ProblemSummary(String name, String description, String example, String solution, ProblemCalculator calculator,
+	public ProblemSummary(ArrayList<String>textStrings, ProblemCalculator calculator,
 			int numberOfInputs, int id)
 	{
-		mName = name;
-		mDescription = description;
-		mExample = example;
-		mSolution = solution;
+		if(textStrings.size() < FIRST_INPUT_INDEX + 1)
+		{
+			return;
+		}
+		mTextStrings = textStrings;
 		mCalculator = calculator;
 		mNumberOfInputs = numberOfInputs;
 		mId = id;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public ProblemSummary(Parcel source)
 	{
-		mName = source.readString();
-		mDescription = source.readString();
-		mExample = source.readString();
-		mSolution = source.readString();
+		mTextStrings = (ArrayList<String>) source.readSerializable();
 		mCalculator = source.readParcelable(ProblemCalculator.class.getClassLoader());
 		mNumberOfInputs = source.readInt();
 		mId = source.readInt();
@@ -37,22 +42,22 @@ public class ProblemSummary implements Parcelable{
 	
 	public String getName()
 	{
-		return mName;
+		return mTextStrings.get(NAME_INDEX);
 	}
 	
 	public String getDescription()
 	{
-		return mDescription;
+		return mTextStrings.get(DESCRIPTION_INDEX);
 	}
 	
 	public String getExample()
 	{
-		return mExample;
+		return mTextStrings.get(EXAMPLE_INDEX);
 	}
 	
 	public String getSolution()
 	{
-		return mSolution;
+		return mTextStrings.get(SOLUTION_INDEX);
 	}
 	
 	public ProblemCalculator getCalculator()
@@ -78,10 +83,7 @@ public class ProblemSummary implements Parcelable{
 	@Override
 	public void writeToParcel(Parcel destination, int flags) {
 		
-		destination.writeString(mName);
-		destination.writeString(mDescription);
-		destination.writeString(mExample);
-		destination.writeString(mSolution);
+		destination.writeSerializable(mTextStrings);
 		destination.writeParcelable(mCalculator, flags);
 		destination.writeInt(mNumberOfInputs);
 		destination.writeInt(mId);
